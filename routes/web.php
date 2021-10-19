dcb8ff1cb8fff812de7554bfca3291be90c01dd7
Marc Geurts <marc.geurts@scarlet.be>
2021-10-19 19:30:11 +0200

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index')->name('home');
-
 Auth::routes();
 
 Route::middleware('auth')->group(function () {
